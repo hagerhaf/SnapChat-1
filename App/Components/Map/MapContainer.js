16a4620b4e6bfa2623240feb10348f84cca6e606
Nathan Malishev <nathan.malishev@gmail.com>
2016-10-17 10:42:03 +1100

    map added and working

--- App/Components/Map/MapContainer.js
+++ App/Components/Map/MapContainer.js
@@ -1,15 +1,52 @@
 import React, {Component, PropTypes} from 'react'
-import {View, Text, MapView} from 'react-native'
-// import MapView from 'react-native-maps'
+import {View, Text} from 'react-native'
+import MapView from 'react-native-maps'
 import {mapStyles as styles} from './MapStyles'
 
 class MapContainer extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }
+    }
+
+    this.onRegionChange = this.onRegionChange.bind(this)
+  }
+
+  componentWillMount () {
+    navigator.geolocation.getCurrentPosition(
+      ({coords, timestamp}) => {
+        this.setState({
+          region: {
+            longitude: coords.longitude,
+            latitude: coords.latitude,
+            latitudeDelta: this.state.latitudeDelta,
+            longitudeDelta: this.state.longitudeDelta
+          }
+        })
+      },
+      (err) => console.log(err),
+      {}
+    )
+  }
+
+  onRegionChange (region) {
+    this.setState({ region })
+  }
+
   render () {
     return (
       <MapView
         style={styles.container}
-        followUserLocation
+        region={this.state.region}
         showsUserLocation
+        onRegionChange={this.onRegionChange}
   />
     )
   }
