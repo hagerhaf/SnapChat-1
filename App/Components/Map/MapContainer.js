97e7ca5622e3eb4bfc507366155f042131e9c441
Nathan Malishev <nathan.malishev@gmail.com>
2016-10-17 22:21:59 +1100

    renders all stories no matter what

--- App/Components/Map/MapContainer.js
+++ App/Components/Map/MapContainer.js
@@ -3,6 +3,7 @@ import {View, Text, MapView, Image, TouchableOpacity} from 'react-native'
 // import MapView from 'react-native-maps'
 import {mapStyles as styles} from './MapStyles'
 import deepcopy from 'deepcopy'
+import getFriends, { getStory } from '../Story/StoryHelpers'
 
 class MapContainer extends Component {
   constructor (props) {
@@ -37,6 +38,7 @@ class MapContainer extends Component {
       (err) => console.log(err),
       {}
     )
+    this.createMarkers(this.props.stories)
   }
 
   componentWillReceiveProps (nextProps) {
@@ -44,20 +46,20 @@ class MapContainer extends Component {
   }
 
   createMarkers (friendsList) {
-    console.log('friendslist', friendsList)
+    let newMarkers = deepcopy(this.state.markers)
+
     friendsList.map((friend) => {
       if (friend.stories) {
         // create an entry for each story
         friend.stories.forEach((story) => {
           if (story.storyInfo.coords) {
-            let newMarkers = deepcopy(this.state.markers)
             newMarkers[story.storyInfo.imageName] = {story, username: friend.username, key: friend.key}
-            this.setState({
-              markers: newMarkers
-            })
           }
         })
       }
+    })
+    this.setState({
+      markers: newMarkers
     })
   }
 
@@ -74,7 +76,7 @@ class MapContainer extends Component {
               this.props.onPressStory([storyObject.story])
             }}>
             <Image
-              style={{width: 30, height: 30}}
+              style={{width: 30, height: 30, borderRadius: 12}}
               source={require('../../../images/added_me.png')}
             />
           </TouchableOpacity>
@@ -88,6 +90,8 @@ class MapContainer extends Component {
   }
 
   render () {
+    console.log('state we got boiz', this.state)
+    console.log('props we got boiz', this.props)
     const annotations = Object.keys(this.state.markers).map((key) => {
       console.log('for images' + key)
       let marker = this.state.markers[key]
